2ea8dc5b6ba7c6a2ec59b0a5e3a265829944fa05
kruceo <rafa2015krueger@gmail.com>
2023-08-25 09:22:34 -0300

    fix: assets otimization

--- src/components/essential/Box.jsx
+++ src/components/essential/Box.jsx
@@ -1,5 +1,5 @@
 import "./Box.less"
 
 export default function Box(props){
-    return <div class={"box " + props.class??''} {...props}>{props.children}</div>
+    return <div {...props} class={"box " + props.class??''}>{props.children}</div>
 }
